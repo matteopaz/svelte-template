0ff4166ac5b5abaf95c57f327265705e91775b06
Matteo Paz <86904497+matteopaz@users.noreply.github.com>
2021-09-18 13:19:59 -0700

    Updated svelte plugin, fixed hmr and compression

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,35 @@
 import { defineConfig, loadEnv } from 'vite';
-import svelte from '@sveltejs/vite-plugin-svelte';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
-import compress from 'vite-plugin-compression';
+import viteCompression from 'vite-plugin-compression';
 
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  return defineConfig({
-    plugins: [
-      svelte({
-        preprocess: sveltePreprocess({
-         sourceMap: true,
-         postcss: {
-           plugins: [
-             require('postcss-nested')(),
-             require('postcss-calc')(),
-             require('postcss-preset-env')({ stage: 2 }),
-             require('autoprefixer')()
-           ]
-         }
-        })
-      }),
-      compress()
-    ],
-    server: {
-        hmr: {
-            port: 443,
+    return defineConfig({
+        plugins: [
+            svelte({
+                preprocess: sveltePreprocess({
+                    sourceMap: true,
+                    postcss: {
+                        plugins: [
+                            require('postcss-nested')(),
+                            require('postcss-calc')(),
+                            require('postcss-preset-env')({ stage: 2 }),
+                            require('autoprefixer')()
+                        ]
+                    }
+                })
+            }),
+            viteCompression({ algorithm: 'brotliCompress', ext: '.br' })
+        ],
+        server: {
+            hmr: {
+                port: 3000,
+                host: "localhost",
+                https: true,
+                open: true
+            }
         }
-    }
-  })
+    })
 }
